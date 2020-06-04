b6c23114c4171e341554cfd06ad1f06e8180cedb
adrsolheim <adrsolheim2@gmail.com>
2020-06-04 21:01:52 +0200

    Add a temporary login form for testing

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
     <h1>Login</h1>
+    <input v-model="username" placeholder="Username">
+    <input v-model="password" type="password" placeholder="Password">
+    <p>U: {{ username }}</p>
+    <p>P: {{ password }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  }
 }
 </script>
